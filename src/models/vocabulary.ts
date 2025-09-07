0687fcef533b9a5a9243c05c63c1618f35fdfa63
thanha17 <148538593+thanha17@users.noreply.github.com>
2025-09-07 23:43:40 +0700

    sửa db

--- src/models/vocabulary.ts
+++ src/models/vocabulary.ts
@@ -1,30 +1,35 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface IVocabulary extends Document {
-  word: string;
-  phonetic: string;
-  type: string;
-  definition: string;
-  examples: string[];
-  image: string;
-  audio: string;
-  created_at: Date;
-  updated_at: Date;
+  word: string; // Từ vựng
+  phonetic: string; // Phiên âm
+  type: string; // Loại từ (noun, verb, adj,...)
+  definition: string; // Định nghĩa
+  examples: string[]; // Ví dụ
+  image: string; // Link ảnh minh họa
+  tags: string[]; // Các tag gắn kèm (ví dụ: TOEIC, Travel, Business)
+  created_at: Date; // Ngày tạo
+  updated_at: Date; // Ngày cập nhật
 }
 
-const VocabularySchema = new Schema<IVocabulary>({
-  word: String,
-  phonetic: String,
-  type: String,
-  definition: String,
-  examples: [String],
-  image: String,
-  audio: String,
-  created_at: { type: Date, default: Date.now },
-  updated_at: Date,
-});
+const VocabularySchema = new Schema<IVocabulary>(
+  {
+    word: { type: String, required: true, trim: true },
+    phonetic: { type: String, trim: true },
+    type: { type: String, trim: true },
+    definition: { type: String, required: true },
+    examples: [{ type: String, trim: true }],
+    image: { type: String, trim: true },
+    tags: [{ type: String, trim: true }],
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now },
+  },
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" }, // Tự động update
+  }
+);
 
 export const Vocabulary = mongoose.model<IVocabulary>(
-  'Vocabulary',
-  VocabularySchema,
+  "Vocabulary",
+  VocabularySchema
 );
