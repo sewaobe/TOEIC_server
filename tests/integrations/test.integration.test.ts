9f0399aa6543a913c58db1dd035a77b67f853dea
baonp <sewaobe@gmail.com>
2026-01-31 08:38:05 +0700

    feat: Update .gitignore and setup jest library for unit & integration test

---
+++ tests/integrations/test.integration.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from '@jest/globals';
+
+describe('Sample Integration Test', () => {
+    it('should pass this sample test', () => {
+        expect(1 + 1).toBe(2);
+    });
+});
