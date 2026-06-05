f4cec83cbf634fc22843ecfa58acc360e8957c23
baonp <sewaobe@gmail.com>
2026-06-05 22:58:10 +0700

    chore(learning-path): Remove unused file

--- src/types/learning_path_v2/index.ts
+++ src/types/learning_path_v2/index.ts
@@ -3,4 +3,3 @@ export * from "./layer1_test_result.types";
 export * from "./layer2_ability_profile.types";
 export * from "./layer3_strategy_decision.types";
 export * from "./layer4_route_optimizer.types";
-export * from "./layer4_scheduler.types";
